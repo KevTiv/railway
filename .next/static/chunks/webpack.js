0683cc8da98dfeced70afdb7721710be323ef5aa
Kevin Tivert <tivertc.kevin@outlook.com>
2022-03-03 11:18:29 +0200

    -mchanged nextjs header

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "875376d94d30259c"; }
+/******/ 		__webpack_require__.h = function() { return "d7c6efacb89c6cee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
